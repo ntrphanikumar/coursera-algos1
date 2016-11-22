a09648517a2bd88797332744c5bf89b4e7778b53
N T R Phani Kumar <ntrphanikumar@gmail.com>
2016-11-22 14:09:22 +0530

    More refined randomized queue impl

--- assignments/week2-queue/src/RandomizedQueue.java
+++ assignments/week2-queue/src/RandomizedQueue.java
@@ -1,75 +1,77 @@
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.Set;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private final Set<WrappedItem> set = new HashSet<>();
+    private int size = 0;
+    private Item[] itemArr = (Item[]) new Object[4];
 
     public boolean isEmpty() {
-        return set.isEmpty();
+        return size == 0;
     }
 
     public int size() {
-        return set.size();
+        return size;
     }
 
     public void enqueue(Item item) {
         if (item == null) {
             throw new NullPointerException();
         }
-        set.add(new WrappedItem(item));
+        if (size < itemArr.length) {
+            itemArr[size++] = item;
+        }
+        resizeIfNeeded();
+    }
+
+    private void resizeIfNeeded() {
+        if ((size == itemArr.length || size < itemArr.length / 4) && size > 0) {
+            Item[] newItemArr = (Item[]) new Object[size * 2];
+            for (int idx = 0; idx < size; idx++) {
+                newItemArr[idx] = itemArr[idx];
+            }
+            itemArr = newItemArr;
+        }
     }
 
     public Item dequeue() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        WrappedItem wi = set.iterator().next();
-        set.remove(wi);
-        return wi.item;
+        int idxToDeque = StdRandom.uniform(size);
+        Item item = itemArr[idxToDeque];
+        itemArr[idxToDeque] = itemArr[--size];
+        itemArr[size] = null;
+        resizeIfNeeded();
+        return item;
     }
 
     public Item sample() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        return set.iterator().next().item;
+        return itemArr[StdRandom.uniform(size)];
     }
 
     public Iterator<Item> iterator() {
         return new Iterator<Item>() {
-            private final Iterator<WrappedItem> itr = set.iterator();
+            private int nextIdx = 0;
 
             @Override
             public boolean hasNext() {
-                return itr.hasNext();
+                return nextIdx < size;
             }
 
             @Override
             public Item next() {
-                return itr.next().item;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return itemArr[nextIdx++];
             }
         };
-    }
-
-    private class WrappedItem {
-        private Item item;
-
-        WrappedItem(Item item) {
-            this.item = item;
-        }
-
-        @Override
-        public String toString() {
-            return item.toString();
-        }
-    }
-
-    @Override
-    public String toString() {
-        return set.toString();
     }
 
     public static void main(String[] args) {
@@ -77,8 +79,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         rq.enqueue("A");
         rq.enqueue("B");
         rq.enqueue("C");
-        while (true) {
-            System.out.println(rq);
-        }
+        rq.enqueue("D");
+        rq.enqueue("E");
+        rq.dequeue();
+        rq.enqueue("F");
+        rq.dequeue();
     }
 }
